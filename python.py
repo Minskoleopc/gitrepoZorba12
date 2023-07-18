a74155844aec287d1a83101cef88c29c674ac592
Minskoleopc <minskoleopc@gmail.com>
2023-07-18 07:40:59 +0530

    added

---
+++ python.py
@@ -0,0 +1,20 @@
+print("hello")
+
+
+
+
+# git status - to track new files and changes over file
+# git init - intialize repo with git
+# git add . - all the files will move to staging
+# git add python.py -   one file from working to staging
+# git commit -m "firstFeature"  (configure email address and username)
+
+
+# go to github and create repository
+# add and make it public
+# create main 
+# connect a local repo  to git  remote repo
+# push the code and check repo
+
+
+
